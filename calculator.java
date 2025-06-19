77112404e6eb3d8277e0314b7c84b0b9195b70b2
Puggy Potato <qingyaurou@gmail.com>
2025-06-19 21:31:12 +0800

    Added a executable file

--- calculator.java
+++ calculator.java
@@ -1,6 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPanel;
+
+
+
+
 public class calculator{
     public static void main(String[] args){
-        int a = 1;
-        System.out.println("I'm Trying Java!");
+        JFrame frame = new JFrame("My GUI Application");
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
