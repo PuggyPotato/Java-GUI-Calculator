51997d88dc67ed5e0b0708f277c7a62381a5e6aa
Puggy Potato <qingyaurou@gmail.com>
2025-06-20 23:49:20 +0800

    Added a textarea for display

--- calculator.java
+++ calculator.java
@@ -1,11 +1,6 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPanel;
-
+import javax.swing.*;
 import java.awt.Color;
-import java.awt.color.*;
+
 
 
 
@@ -13,12 +8,17 @@ import java.awt.color.*;
 public class calculator{
     public static void main(String[] args){
         JFrame frame = new JFrame("My Calculator!");
-        frame.setSize(2000,2000);
+        JTextArea display = new JTextArea("");
+        frame.setSize(1000,2000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setLayout(null);
 
-        frame.getContentPane().setBackground(Color.lightGray);
+        display.setBackground(Color.LIGHT_GRAY);
+        display.setBounds(10,20,950,80);
+
+        frame.getContentPane().setBackground(Color.gray);
+        frame.add(display);
         
     }
 }
