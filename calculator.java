06a1b7d03b50c1471bfff5005011473d853e6393
Puggy Potato <qingyaurou@gmail.com>
2025-06-20 23:44:01 +0800

    Added a background colour

--- calculator.java
+++ calculator.java
@@ -4,14 +4,21 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.color.*;
+
 
 
 
 public class calculator{
     public static void main(String[] args){
-        JFrame frame = new JFrame("My GUI Application");
-        frame.setSize(400,400);
+        JFrame frame = new JFrame("My Calculator!");
+        frame.setSize(2000,2000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setLayout(null);
+
+        frame.getContentPane().setBackground(Color.lightGray);
+        
     }
 }
